8d1f0bf6d46d4ca66b65016ed736b7cb84b482d4
roba2024 <roba082024@gmail.com>
2024-09-06 10:31:25 +0200

    first.3/7

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace uke1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int heiltall = 250;
+        Console.WriteLine(heiltall);
+        string tekst = "uke1";
+        Console.WriteLine(tekst);
+        double dtall = 250.14159;
+        Console.WriteLine(dtall);
+
+    }
+}
