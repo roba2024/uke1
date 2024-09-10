4cfc7adc3f646a38102245c176922f5b82ae47b8
roba2024 <roba082024@gmail.com>
2024-09-10 12:34:29 +0200

    andre opplasting

--- Program.cs
+++ Program.cs
@@ -11,6 +11,18 @@ class Program
         Console.WriteLine(tekst);
         double dtall = 250.14159;
         Console.WriteLine(dtall);
+        char bokstav = 'r';
+        Console.WriteLine(bokstav);
+        string[] tall = {"0","1","2","3","4"};
+        Console.WriteLine(tall.Length);
+        for (int i = 0; i < tall.Length; i++)
+        {
+         Console.WriteLine(tall[i]);
 
+        }
+        foreach (string item in tall)
+        {
+         Console.WriteLine(item); 
+        }
     }
 }
--- obj/Debug/net8.0/uke1.AssemblyInfo.cs
+++ obj/Debug/net8.0/uke1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("uke1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d1f0bf6d46d4ca66b65016ed736b7cb84b482d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("uke1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("uke1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
